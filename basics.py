2b888e878a34a3fba28ac2b21c6784163fd98668
Michael Callahan <mcallahan0@yahoo.com>
2022-02-08 18:26:57 -0500

    Pushing initial code

---
+++ basics.py
@@ -0,0 +1,25 @@
+# Coding Exercise 2:
+# 1. Numbers: Example code to add two numbers 50+50 and 100-10 and print it.
+
+print(50 + 50)
+print(100 - 10)
+
+# 2. 30+*6, 6^6, 6**6, 6+6+6+6+6+6
+
+# 30+*6 throws a syntax error
+# print(30+*6)
+
+# 6^6 outputs 0.
+print(6^6)
+
+# 6**6 outputs 46656
+print(6**6)
+
+# 6+6+6+6+6+6 outputs 36
+print(6+6+6+6+6+6)
+
+# 3. Print "Hello World" on the console output. Print output "Hello World : 10"
+# Make sure capitalization and spacing matches.
+
+print("Hello World")
+print("Hello World : 10")
