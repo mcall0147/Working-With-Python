304b0d6311186ba71a4a5443509b817fb198c47d
Michael Callahan <mcallahan0@yahoo.com>
2022-02-09 17:06:40 -0500

    Set up the mortgage problem and assigned values, set up a for loop and printed the final amount after the loan is paid off

--- basics.py
+++ basics.py
@@ -1,3 +1,5 @@
+import math
+
 # Coding Exercise 2:
 # 1. Numbers: Example code to add two numbers 50+50 and 100-10 and print it.
 
@@ -23,3 +25,33 @@ print(6+6+6+6+6+6)
 
 print("Hello World")
 print("Hello World : 10")
+
+# 4. Mathematically, mortgage works by the following algorithm:
+
+# Interest rate
+r = 0.06
+# Length of time in months
+l = 103
+
+def calculateAmount(length):
+    # Loan amount
+    p = 800000
+    # Monthly payment amount
+    m = 10000 
+
+    # For loop to reduce the total amount owed after each month.
+    for i in range(l - 1):
+        interest = math.ceil(p) * (r/12)
+        p += math.ceil(interest)
+        p -= m
+
+    # Setting monthly payment to the remaining amount below 10,000
+    m = p
+    # Subtracting final monthly payment from remaining loan amount.
+    p -= m
+    
+    # Printing the final amount of 0 after the loan has been paid off over the 103 months
+    print(p)
+
+# Calling the function with the parameter l for the number of months
+calculateAmount(l)
